f9a0f7001671589910b7f11feafb2367e6f77819
Gertjan van Oosten <gertjan@west.nl>
2025-03-13 11:40:47 +0100

    Use StAX instead of DocumentBuilder

--- src/main/java/io/github/borewit/sanitize/SVGSanitizer.java
+++ src/main/java/io/github/borewit/sanitize/SVGSanitizer.java
@@ -1,132 +1,114 @@
 package io.github.borewit.sanitize;
 
-import org.w3c.dom.*;
-
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 
 public class SVGSanitizer {
 
-  private final DocumentBuilderFactory factory;
+  private static final Set<Integer> UNSAFE_EVENT_TYPES =
+      Set.of(XMLStreamConstants.DTD, XMLStreamConstants.ENTITY_REFERENCE);
 
-  public SVGSanitizer() {
-    try {
-      this.factory = makeDocumentBuilderFactory();
-    } catch (ParserConfigurationException e) {
-      throw new RuntimeException("Failed to initialize DocumentBuilderFactory", e);
-    }
-  }
+  private static final Set<String> UNSAFE_ELEMENTS =
+      Set.of("script", "foreignObject", "iframe", "embed", "object", "style");
 
-  private static DocumentBuilderFactory makeDocumentBuilderFactory() throws ParserConfigurationException {
-    final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    factory.setValidating(false);
-    factory.setNamespaceAware(true);
-    factory.setFeature("http://xml.org/sax/features/namespaces", true);
-    factory.setFeature("http://xml.org/sax/features/validation", false);
-    factory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
-    factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-
-    // Prevents exception: External Entity: Failed to read external document
-    factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
-
-    // Do not include external parameter entities or the external DTD subset.
-    // Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-parameter-entities
-    // Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-parameter-entities
-    factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-
-    // Disable external DTDs as well
-    // Xerces 2 - https://xerces.apache.org/xerces2-j/features.html#nonvalidating.load-external-dtd
-    factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-
-    factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-    factory.setXIncludeAware(false);
-
-    return factory;
-  }
-
-  private static final Set<String> UNSAFE_ELEMENTS = new HashSet<>(Arrays.asList(
-    "script", "foreignObject", "iframe", "embed", "object", "xhtml:script", "style"
-  ));
-
-  private static final Set<String> UNSAFE_ATTRIBUTES = new HashSet<>(Arrays.asList(
-    "onload", "onclick", "onmouseover", "onerror", "onfocus", "onblur", "onkeydown"
-  ));
+  private static final Set<String> UNSAFE_ATTRIBUTES =
+      Set.of("onload", "onclick", "onmouseover", "onerror", "onfocus", "onblur", "onkeydown");
 
   public String sanitize(String svgContent) throws Exception {
-    try (ByteArrayInputStream inputStream = new ByteArrayInputStream(svgContent.getBytes(StandardCharsets.UTF_8));
-         ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-      this.sanitize(inputStream, outputStream);
+    try (
+        final ByteArrayInputStream inputStream =
+            new ByteArrayInputStream(svgContent.getBytes(StandardCharsets.UTF_8));
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+      sanitize(inputStream, outputStream);
       return outputStream.toString(StandardCharsets.UTF_8);
     }
   }
 
-  public void sanitize(InputStream is, OutputStream os) throws Exception {
-    final DocumentBuilder builder = this.factory.newDocumentBuilder();
-    Document doc = builder.parse(is);
+  public static void sanitize(InputStream is, OutputStream os) throws Exception {
+    final XMLInputFactory factory = XMLInputFactory.newInstance();
+    // Disable DTDs entirely for the factory
+    factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+    // Ignore unknown entities
+    factory.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, false);
 
-    removeUnsafeElements(doc);
-    removeUnsafeAttributes(doc.getDocumentElement());
+    final XMLEventReader eventReader = factory.createXMLEventReader(is);
+    final XMLOutputFactory outFactory = XMLOutputFactory.newInstance();
+    final XMLEventWriter eventWriter = outFactory.createXMLEventWriter(os);
+    final XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 
-    final TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    transformerFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-    // Check OWASP recommendations XMLConstants.ACCESS_EXTERNAL_DTD & XMLConstants.ACCESS_EXTERNAL_STYLESHEET (are set with XMLConstants.FEATURE_SECURE_PROCESSING)
-    // https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html#transformerfactory
-    assert ("".equals(transformerFactory.getAttribute(XMLConstants.ACCESS_EXTERNAL_DTD)));
-    assert ("".equals(transformerFactory.getAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET)));
-
-    final Transformer transformer = transformerFactory.newTransformer();
-    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-
-    final DOMSource source = new DOMSource(doc);
-    StreamResult result = new StreamResult(os);
-    transformer.transform(source, result);
-  }
-
-  private static void removeUnsafeElements(Document doc) {
-    for (String tag : UNSAFE_ELEMENTS) {
-      final NodeList nodeList = doc.getElementsByTagName(tag);
-      for (int i = nodeList.getLength() - 1; i >= 0; i--) {
-        final Node node = nodeList.item(i);
-        if (node.getParentNode() != null) {
-          node.getParentNode().removeChild(node);
+    while (eventReader.hasNext()) {
+      final XMLEvent event = eventReader.nextEvent();
+      if (UNSAFE_EVENT_TYPES.contains(event.getEventType())) {
+        continue;
+      }
+      if (!event.isStartElement()) {
+        // Not a start element, write it
+        eventWriter.add(event);
+      } else {
+        // Check if this is the start of an element we want to remove
+        final StartElement startElement = event.asStartElement();
+        if (UNSAFE_ELEMENTS.contains(startElement.getName().getLocalPart())) {
+          skipElementAndChildren(eventReader);
+        } else {
+          // Check attributes and write sanitized element
+          eventWriter.add(getElementWithSanitizedAttributes(startElement, eventFactory));
         }
       }
     }
   }
 
-  private static void removeUnsafeAttributes(Element element) {
-    final NamedNodeMap attributes = element.getAttributes();
-    for (int i = attributes.getLength() - 1; i >= 0; i--) {
-      final Node attr = attributes.item(i);
-      String attrName = attr.getLocalName().toLowerCase();
-      String attrValue = attr.getNodeValue().toLowerCase();
-
-      if (UNSAFE_ATTRIBUTES.contains(attrName) || attrValue.startsWith("javascript:")) {
-        element.removeAttribute(attr.getNodeName());
-      }
-
-      if ("href".equals(attrName) && !attrValue.startsWith("data:")) {
-        element.removeAttribute(attr.getNodeName());
-      }
-    }
-
-    NodeList children = element.getChildNodes();
-    for (int i = 0; i < children.getLength(); i++) {
-      final Node node = children.item(i);
-      if (node instanceof Element) {
-        removeUnsafeAttributes((Element) node);
+  private static void skipElementAndChildren(XMLEventReader eventReader) throws XMLStreamException {
+    // Skip this element and all its children by tracking depth
+    int depth = 1;
+    while (eventReader.hasNext() && depth > 0) {
+      final XMLEvent ignoredEvent = eventReader.nextEvent();
+      if (ignoredEvent.isStartElement()) {
+        depth++;
+      } else if (ignoredEvent.isEndElement()) {
+        depth--;
       }
     }
   }
+
+  private static StartElement getElementWithSanitizedAttributes(StartElement startElement,
+      XMLEventFactory eventFactory) {
+    final Iterator<Attribute> attributes = startElement.getAttributes();
+    final List<Attribute> sanitizedAttributes = new ArrayList<>();
+    boolean foundOffendingAttributes = false;
+    while (attributes.hasNext()) {
+      final Attribute attr = attributes.next();
+      final String attrName = attr.getName().getLocalPart();
+      final String attrValue = attr.getValue().toLowerCase();
+      if (UNSAFE_ATTRIBUTES.contains(attrName) || attrValue.startsWith("javascript:")
+          || ("href".equals(attrName) && !attrValue.startsWith("data:"))) {
+        foundOffendingAttributes = true;
+      } else {
+        sanitizedAttributes.add(attr);
+      }
+    }
+    return foundOffendingAttributes
+        // Create a new StartElement without the unwanted attributes
+        ? eventFactory.createStartElement(startElement.getName(), sanitizedAttributes.iterator(),
+            startElement.getNamespaces())
+        // Element was fine
+        : startElement;
+  }
+
 }
